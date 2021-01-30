25535a7f26f326a4a4a87129fa7d9936cb0e71bc
Kimberley Smith <kimberley.smith@gamesmiths.com.au>
2021-01-30 23:31:44 +0800

    set up metal detector making noise

---
+++ Assets/Scripts/Sound/INote.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface INote
+{
+    void SetStrength(float strength);
+
+    void ChangeFrequency(float frequency);
+}
