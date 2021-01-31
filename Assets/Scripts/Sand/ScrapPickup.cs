86bd1ce91f624b3d19cee284f32cf6edb6543fdf
AMarcuolaBel <aramis@tunedoutgames.com>
2021-01-31 14:43:18 +0800

    Prefab fixes

--- Assets/Scripts/Sand/ScrapPickup.cs
+++ Assets/Scripts/Sand/ScrapPickup.cs
@@ -12,12 +12,35 @@ public class ScrapPickup : MonoBehaviour
 
     IMetal scrap;
 
+    XRGrabInteractable interactable;
+
+    Rigidbody rb;
+
+    bool isPicked = false;
+
 
     private void Start()
     {
         scrap = GetComponent<IMetal>();
         audioSource = GetComponent<AudioSource>();
+        ObjectCollector.instance.SetIntemNumber(ScrapMetal.GetFoundText());
+        ObjectCollector.instance.SetTotalValue(ScrapMetal.GetTotalValue());
+        interactable = GetComponent<XRGrabInteractable>();
+        rb = GetComponent<Rigidbody>();
     }
+
+    private void Update()
+    {
+        if(interactable.isSelected && !isPicked)
+        {
+            isPicked = true;
+            Pickup();
+            rb.useGravity = true;
+        }
+
+        
+    }
+
 
     public void Pickup()
     {
